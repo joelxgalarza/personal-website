379aa44403da906112b7bc8a11b01589a64016c4
Joel Galarza <swe@joelgalarza.com>
2024-02-16 01:13:21 -0500

    formatted navigation bar

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,9 +1,13 @@
 import NavigationCSS from './Navigation.module.css'
 
 function Navigation(){
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Saira+Extra+Condensed:wght@100;200;300;400;500;600;700;800;900&display=swap')
+    </style>
+
     return(
         <div className={NavigationCSS.container}>
-            <ul>
                 <img src="/public/pfp.png" alt="" className={NavigationCSS.pfp}/>
                 <p className={NavigationCSS.navLink}>About</p>
                 <p className={NavigationCSS.navLink}>Experience</p>
@@ -11,7 +15,6 @@ function Navigation(){
                 <p className={NavigationCSS.navLink}>Project</p>
                 <p className={NavigationCSS.navLink}>Skills</p>
                 <p className={NavigationCSS.navLink}>Awards</p>
-            </ul>
         </div>
     )
 }
